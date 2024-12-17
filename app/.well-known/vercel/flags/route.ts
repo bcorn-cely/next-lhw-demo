d009590baeb5275f265c065b14551cc2e31395f6
Brandon Corn <brandon.corn@vercel.com>
2024-12-16 23:29:06 -0500

    add /.well-known for feature flags

---
+++ app/.well-known/vercel/flags/route.ts
@@ -0,0 +1,40 @@
+import { NextResponse, type NextRequest } from 'next/server';
+import { verifyAccess, type ApiData } from '@vercel/flags';
+ 
+export async function GET(request: NextRequest) {
+  const access = await verifyAccess(request.headers.get('Authorization'));
+  if (!access) return NextResponse.json(null, { status: 401 });
+ 
+  return NextResponse.json<ApiData>({
+    definitions: {
+      showLuxuryExperiences: {
+				description: 'Controls whether the new feature is visible',
+				options: [
+					{ value: false, label: 'Off' },
+					{ value: true, label: 'On' },
+				],
+      },
+			enableVirtualConcierge: {
+				description: 'Controls visibility of concierge',
+				options: [
+					{ value: false, label: 'Off' },
+					{ value: true, label: 'On' }
+				]
+			},
+			offerExclusiveRates: {
+				description: 'Controls the visibility of exclusive rate offerings',
+				options: [
+					{ value: false, label: 'Off' },
+					{ value: true, label: 'On' }
+				]
+			},
+			showPersonalizedRecommendations: {
+				description: 'Shows personalized recommendations',
+				options: [
+					{ value: false, label: 'Off' },
+					{ value: true, label: 'On' }
+				]
+			},
+    },
+  });
+}
